3092c6030e39eff9dedd614429a4f5b3307756d6
thongtran89 <tranhuuquocthong@gmail.com>
2023-07-05 17:09:33 +0700

    test

--- scenarios/perf_test_scenarios.js
+++ scenarios/perf_test_scenarios.js
@@ -5,9 +5,9 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 // Test setup
 export let options = {
     stages: [
-        { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-        { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes.
-        { duration: '5m', target: 0 }, // ramp-down to 0 users.
+        { duration: '30s', target: 5 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+        { duration: '20s', target: 5 }, // stay at 100 users for 10 minutes.
+        { duration: '10s', target: 0 }, // ramp-down to 0 users.
     ]
 };
 
